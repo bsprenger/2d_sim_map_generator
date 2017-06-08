32ea59835eb2cd54c0a0493a0645c1bd95dd9719
Ben Sprenger <bensprenger@Bens-MacBook-Pro.local>
2017-06-08 12:22:59 -0400

    adding support for multiple states

--- cellularautomata.cpp
+++ cellularautomata.cpp
@@ -1,4 +1,17 @@
 
+//todo: fix map.length things to proper c++ syntax
+
+/* RULES
+if a wall/ramp has less than 2 similar neighbours, it dies
+----to implement if having problems------
+if a wall/ramp has 2 or 3 similar neighbours, it stays alive
+if a wall/ramp has more than 3 similar neighbours it dies.
+-----------------------------------------
+if an open space or hole has 3 neighbours that are all wall or all ramp, it becomes that.
+
+
+*/
+
 #include "mainwindow.h"
 
 public mapVector initialiseMap(mapVector map) {
@@ -34,6 +47,12 @@ public mapVector initialiseMap(mapVector map) {
 
 	return map;
 }
+
+
+
+
+
+
 
 public mapVector doSimulationStep(mapVector oldMap) {
 	mapVector newMap = new mapVector[width][height];
@@ -80,9 +99,16 @@ public mapVector doSimulationStep(mapVector oldMap) {
 }
 
 
-public int countAlikeNeighbours(mapVector map, int x, int y) {
 
-	int count = 0;
+
+
+
+
+//for wall and ramp to see whether they live or die
+public int countAlikeNeighbours(mapVector map, int x, int y) {
+//implement separate part for between (1,2]
+	int alikeCount = 0;
+
 	for (int i =-1; i<2; i++) {
 		for (int j = -1; j<2; j++) {
 			int neighbour_x = x+i;
@@ -95,14 +121,77 @@ public int countAlikeNeighbours(mapVector map, int x, int y) {
 			else if(neighbour_x < 0 || neighbour_y < 0 || neighbour_x >= map.length || neighbour_y >= map[0].length){
 				//count = count + 1;
 			}
-			//otherwise, a normal check of the neighbour
+			//otherwise, a normal check of the neighbour to see if alike
 			else if (map[neighbour_x][neighbour_y] == map[x][y]) {
-				count = count + 1;
+				alikeCount++;
 			}
 		}
 	}
 	return count;
 }
+
+
+
+
+
+
+
+
+//count amount of walls and ramps
+//compare which neighbour is highest. then, if over a threshold, return the type. in the receiver, if return is the type, change it to that. else, keep it the same
+public int countHighestNeighbour(mapVector map, int x, int y) {
+	
+	int wallCount = 0;
+	int rampCount = 0;
+
+	for (int i =-1; i<2; i++) {
+		
+		for (int j = -1; j<2; j++) {
+			int neighbour_x = x+i;
+			int neighbour_y = y+j;
+			
+			//if we're looking at the middle point
+			if(i==0 && j == 0) {
+				//do nothing
+			}
+			
+			else if (neighbour_x < 0 || neighbour_y < 0 || neighbour_x >= map.length || neighbour_y >= map[0].length) {
+				//off the edge of the map, do nothing
+			}
+			
+			else {
+				// if a wall, add to wall count. if a ramp, add to ramp count. we don't care about other types
+				if (map[neighbour_x][neighbour_y] == -2) {
+					wallCount++; //check if this is proper syntax
+				}
+				else if (map[neighbour_x][neighbour_y] <= 2 && map[neighbour_x][neighbour_y] > 1) {
+					rampCount++;
+				}
+			}
+		}
+	}
+	// TODO : combine if statements
+	if (wallCount > rampCount) {
+		if (wallCount >= 3) {
+			return -2; //check syntax for this
+		}
+	}
+	else if (rampCount > wallCount) {
+		if (rampCount > 3) {
+			return 2;
+		}
+	}
+	else if (rampCount == wallCount)
+}
+
+
+
+
+
+
+
+
+
 
 public mapVector generateMap(){
 	//create a new map
@@ -115,24 +204,4 @@ public mapVector generateMap(){
 	}
 	return 
 }
-
-public void countHighestNeighbour(mapVector map, int x, int y) {
-//must go through neighbours, and keep a tally of the numbers of each type. then, return the number of highest type and the tally of that number
-
-	int count = 0;
-	for (int i =-1; i<2; i++) {
-		for (int j = -1; j<2; j++) {
-			int neighbour_x = x+i;
-			int neighbour_y = y+j;
-			//if we're looking at the middle point
-			if(i==0 && j == 0) {
-				//do nothing
-			}
-			else if (neighbour_x < 0 || neighbour_y < 0 || neighbour_x >= map.length || neighbour_y >= map[0].length) {
-				
-			}
-		}
-	}
-}
-
 
